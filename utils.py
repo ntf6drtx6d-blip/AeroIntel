60e6e496714802677628c3ed5ecd605c935d068e
sala_dev <dkucheruk@gmail.com>
2026-03-24 21:16:51 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
+def normalize_airport(name):
+    if not name:
+        return "Unknown"
 
+    name = name.lower()
+
+    remove_words = [
+        "airport",
+        "international",
+        "aeropuerto",
+        "aeroport"
+    ]
+
+    for word in remove_words:
+        name = name.replace(word, "")
+
+    return name.strip().title()
