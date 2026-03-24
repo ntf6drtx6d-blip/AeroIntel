52ca8b9c0de595966f512d522fd506e5d6eef176
sala_dev <dkucheruk@gmail.com>
2026-03-24 21:17:39 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,34 @@
+import feedparser
+import json
+from config import RSS_FEEDS
+from ai_engine import extract_signals
+from db import save_signal, signal_exists
 
+
+def fetch_news():
+
+    for url in RSS_FEEDS:
+        feed = feedparser.parse(url)
+
+        for entry in feed.entries[:5]:
+
+            text = entry.title + " " + entry.summary
+
+            result = extract_signals(text)
+
+            try:
+                data_list = json.loads(result)
+
+                for d in data_list:
+
+                    if not d.get("relevant"):
+                        continue
+
+                    if d.get("confidence", 0) < 50:
+                        continue
+
+                    if not signal_exists(d.get("signal")):
+                        save_signal(d, entry.link)
+
+            except:
+                continue
