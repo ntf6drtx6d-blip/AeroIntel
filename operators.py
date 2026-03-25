60ccf661482b9157a75bbcf24d2462e44df840a0
sala_dev <dkucheruk@gmail.com>
2026-03-25 11:42:21 +0100

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,1 +1,11 @@
-
+KNOWN_OPERATORS = {
+    "infraero": "Infraero",
+    "fraport": "Fraport",
+    "vinci": "Vinci Airports",
+    "aena": "AENA",
+    "adp": "Groupe ADP",
+    "zurich airport": "Zurich Airport",
+    "egis": "Egis",
+    "vega": "Vega",
+    "red vol": "Red Vol",
+}
