fe76539774b703938dbb4f145532e6f13a7770fa
sala_dev <dkucheruk@gmail.com>
2026-03-25 12:24:45 +0100

    Update operator_sources.py

--- operator_sources.py
+++ operator_sources.py
@@ -1,1 +1,95 @@
-
+OPERATOR_SOURCES = {
+    "Brazil": [
+        {
+            "name": "AENA Brazil",
+            "type": "private_operator",
+            "url": "https://www.aena.com.br/en/",
+        },
+        {
+            "name": "VINCI Airports Brazil",
+            "type": "private_operator",
+            "url": "https://www.vinci-airports.com/en/airports/brazil/",
+        },
+        {
+            "name": "Fraport Brazil",
+            "type": "private_operator",
+            "url": "https://www.fraport-brasil.com/en/",
+        },
+        {
+            "name": "CCR Aeroportos",
+            "type": "private_operator",
+            "url": "https://www.ccraeroportos.com.br/",
+        },
+        {
+            "name": "Rede Voa",
+            "type": "private_operator",
+            "url": "https://redevoa.com.br/",
+        },
+        {
+            "name": "BH Airport",
+            "type": "private_operator",
+            "url": "https://www.bhairport.com.br/en/",
+        },
+        {
+            "name": "RioGaleao",
+            "type": "private_operator",
+            "url": "https://www.riogaleao.com/en/",
+        },
+        {
+            "name": "GRU Airport",
+            "type": "private_operator",
+            "url": "https://www.gru.com.br/en",
+        },
+        {
+            "name": "Inframerica",
+            "type": "private_operator",
+            "url": "https://www.inframerica.aero/en/",
+        },
+    ],
+    "Mexico": [
+        {
+            "name": "OMA",
+            "type": "private_operator",
+            "url": "https://www.oma.aero/en/",
+        },
+        {
+            "name": "ASUR",
+            "type": "private_operator",
+            "url": "https://www.asur.com.mx/",
+        },
+        {
+            "name": "GAP",
+            "type": "private_operator",
+            "url": "https://www.aeropuertosgap.com.mx/en/",
+        },
+    ],
+    "Colombia": [
+        {
+            "name": "OPAIN",
+            "type": "private_operator",
+            "url": "https://www.opain.co/en/",
+        },
+        {
+            "name": "Airplan",
+            "type": "private_operator",
+            "url": "https://www.airplan.aero/",
+        },
+    ],
+    "Australia": [
+        {
+            "name": "Sydney Airport",
+            "type": "private_operator",
+            "url": "https://www.sydneyairport.com.au/corporate",
+        },
+        {
+            "name": "Brisbane Airport",
+            "type": "private_operator",
+            "url": "https://www.bne.com.au/corporate",
+        },
+        {
+            "name": "Melbourne Airport",
+            "type": "private_operator",
+            "url": "https://corporate.melbourneairport.com.au/",
+        },
+    ],
+}
